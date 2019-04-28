56b7f0e22df205890065d0e06e480cdb5c0f9a72
bogomilmihaylov <bogomilmihaylov@live.com>
2019-04-28 16:38:58 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,7 @@
+var playlist = 'artist':"Alienata","Intergalactic"};
+playlist.artist = 'TLR'
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist{artistName,songTitle}
+  return obj
+}
