f4b64e5d0b7765f53bef7af145e7b49b791cd014
bogomilmihaylov <bogomilmihaylov@live.com>
2019-04-28 16:41:33 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@ var playlist = 'artist':"Alienata","Intergalactic"};
 playlist.artist = 'TLR'
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName]=songTitle}
+  playlist[artistName]=songTitle
   return obj
 }
